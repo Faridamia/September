0907ff88d0e4441c53f0d91fba2d3213c5c6aa8a
Your Name <youremail@yourdomain.com>
2023-10-01 01:30:19 +0600

    commit

--- bin/september.dart
+++ bin/september.dart
@@ -1,5 +1,83 @@
-import 'package:september/september.dart' as september;
+void main() {}
+  
+  
+  
+  
+  
+  //print(getSeason(6));
+//}
 
-void main(List<String> arguments) {
-  print('Hello world: ${september.calculate()}!');
-}
+//String getSeason(int month) {
+  //String resualt = '';
+  //if (month == 1 || month == 2 || month == 12) {
+    //resualt = 'Зима';
+  //} else if (month == 3 || month == 4 || month == 5) {
+    //resualt = 'Весна';
+  //} else if (month == 6 || month == 7 || month == 8) {
+    //resualt = 'Лето';
+  //} else if (month == 9 || month == 10 || month == 11) {
+   // resualt = 'Осень';
+  //} else {
+  //  resualt = 'error';
+  //}
+  //return resualt;
+//}
+
+//{
+  //print(getSimpleNumber(7),);
+//}
+
+//bool getSimpleNumber(int number) {
+  //if (number % 1 == 0 && number % number == 0) {
+    //return true;
+  //} else {
+    //return false;
+  //}
+//}
+
+
+
+
+// {
+  //getType(object: 1.9);
+//}
+
+//void getType({required dynamic object}) {
+  //if (object is String) {
+   // print('Строка');
+ // } else if (object is int) {
+    //print('Целое число');
+  //} else if (object is double) {
+    //print('Дробное число');
+  //} else {
+    //print('Не удалось определить тип');
+  //}
+//}
+
+
+
+
+
+//{
+  //print(getStringLeght(string: 'string'));
+//}
+
+//int getStringLeght({required String string}) {
+  //return string.length;
+//}
+
+
+
+
+
+//{
+ //print(funcOne(firstNum: 10, secondNum: 3));
+//}
+
+//String funcOne({required int firstNum, required int secondNum}) {
+//if (firstNum % secondNum == 0) {
+//return 'Делится нацело';
+//} else {
+ //return 'Не делится нацело';
+ //}
+//}
